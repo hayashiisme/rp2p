2c0e03c1af37978f67bb992a53c21d23adbac288
Isme Hayashi <linquanstudio@gmail.com>
2019-05-28 00:10:22 +0900

    update

--- rp2p.py
+++ rp2p.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+import random
+import array
+
+random.seed()
+while True:
+    d = int(random.random() * 16.0)
+    if d > 3: break
+print("{} points".format(d))
+aa = []
+for n in range(d): aa.append(array.array('d', [ random.random(), random.random() ]))
+for a in aa: print(a)
+
+    
