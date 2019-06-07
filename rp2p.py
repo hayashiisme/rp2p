771bb8fe4d07ea6ebb8d13bc2320a0010da96ad7
坂本浩 <sakamoto@kswdh177130.ksw.aist.go.jp>
2019-06-07 15:03:58 +0900

    update

--- rp2p.py
+++ rp2p.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import random
 import array
+import math
+import matplotlib.pyplot as plt
 
 random.seed()
 while True:
@@ -11,4 +13,25 @@ aa = []
 for n in range(d): aa.append(array.array('d', [ random.random(), random.random() ]))
 for a in aa: print(a)
 
+# plot
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+for a in aa: ax.plot(a[0], a[1], 'g.', markersize=10)
+plt.tight_layout()
+#plt.savefig(op, dpi=300)
+plt.show()
+
+
+
     
+# 最初の点との組み合わせで2点間の距離を取る
+dists = []
+for i in range(len(aa) - 1):
+    for j in range(i + 1, len(aa)):
+        x1 = aa[i][0]
+        y1 = aa[i][1]
+        x2 = aa[j][0]
+        y2 = aa[j][1]
+        dists.append([math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), i, j])
+
+for dist in sorted(dists): print(dist)
+
