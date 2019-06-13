f75a075e2f06e48136a938254175c629a7811702
坂本浩 <sakamoto@kswdh177130.ksw.aist.go.jp>
2019-06-13 20:04:29 +0900

    update

--- rp2p.py
+++ rp2p.py
@@ -9,29 +9,53 @@ while True:
     d = int(random.random() * 16.0)
     if d > 3: break
 print("{} points".format(d))
-aa = []
-for n in range(d): aa.append(array.array('d', [ random.random(), random.random() ]))
-for a in aa: print(a)
 
 # plot
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-for a in aa: ax.plot(a[0], a[1], 'g.', markersize=10)
+
+i_minx = -1
+xa = []
+ya = []
+
+for n in range(d):
+    x = random.random()
+    if i_minx < 0:
+        i_minx = n
+    else:
+        if xa[i_minx] >= x:
+            i_minx = n
+    xa.append(x)
+    ya.append(random.random())
+
+for n in range(d):
+    print(xa[n], ya[n])
+    s = 'r.' if n == i_minx else 'g.'
+    ax.plot(xa[n], ya[n], s, markersize=10)
+
+# 外側に輪ゴムをかける
+i_mind = -1
+cosm = -1
+for n in range(d):
+    if n == i_minx: continue
+    # 角度を求めて
+    if i_mind == -1:
+        i_mind = n
+        v0 = ( xa[i_minx], ya[i_minx] - 1 )
+        va = ( xa[n] - xa[i_minx], ya[n] - ya[i_minx] )
+        cosm = v0[0] * va[0] + v0[1] * va[1]
+    else:
+        v0 = ( xa[i_minx], ya[i_minx] - 1 )
+        va = ( xa[n] - xa[i_minx], ya[n] - ya[i_minx] )
+        cosa = v0[0] * va[0] + v0[1] * va[1]
+        if cosm > cosa:
+            i_mind = n
+            cosm = cosa
+
+ax.plot([ xa[i_minx], xa[i_mind] ], [ ya[i_minx], ya[i_mind] ])
+            
 plt.tight_layout()
-#plt.savefig(op, dpi=300)
 plt.show()
 
 
 
     
-# 最初の点との組み合わせで2点間の距離を取る
-dists = []
-for i in range(len(aa) - 1):
-    for j in range(i + 1, len(aa)):
-        x1 = aa[i][0]
-        y1 = aa[i][1]
-        x2 = aa[j][0]
-        y2 = aa[j][1]
-        dists.append([math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2), i, j])
-
-for dist in sorted(dists): print(dist)
-
